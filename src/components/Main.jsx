a37203828fc441b260b90abed877658414105aa5
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-09-08 17:55:46 -0400

    added styling for main contenct

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Main() {
     return (
-        <div>
-            <h1>Fun Facts About React</h1>
-            <ul>
+        <div className="main--container">
+            <h1 className="main--title">Fun Facts About React</h1>
+            <ul className="main--facts">
                 <li>Was first Released in 2013</li>
                 <li>Was originall created by Jordan Walke</li>
                 <li>Has well over 100K stars on Github</li>
