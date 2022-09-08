a514f805d38a4ffe2f7b41085435fbfa3fabd806
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-09-08 17:39:22 -0400

    styled the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Navbar from "./components/Navbar"
 export default function App() {
     return (
       <div>
-        <h1 className="title">Hello World!</h1>
         <Navbar/>
         <Main/>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import icon from "../assets/images/react-icon-small.png"
 export default function Main() {
     return (
         <nav>
-            <img src={icon} alt="react small icon" />  
-            <p>ReactFacts</p>  
-            <p>React Course - Project 1</p>
+            <img src={icon} alt="react small icon" className="nav--icon" />  
+            <p className="nav--icon_text">ReactFacts</p>  
+            <p className="nav--title">React Course - Project 1</p>
         </nav>
     )
 }
